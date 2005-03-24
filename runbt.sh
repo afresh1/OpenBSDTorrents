9073f70eb5b410fb2f9c54c3044ca9c35954ae8c
andrew <andrew>
2005-03-24 19:37:48 +0000

    made it a real script that will launch itself again on failure

--- runbt.sh
+++ runbt.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+#$Id$
+
 btlaunchmany.py --super_seeder 1 --max_uploads 28 \
     --minport 10000 --maxport 10500 \
     --saveas /home/ftp/pub/ --saveas_style 2 \
     /home/andrew/torrents/
+
+/home/andrew/OpenBSDTorrents/runbt.sh
