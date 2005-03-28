5fc2949a551c0d557c3720d72385a003a40fcf87
andrew <andrew>
2005-03-28 23:39:40 +0000

    lots of changes.  mostly just made it work now

--- regen.sh
+++ regen.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-MakeTorrents.pl $@
-CurrentTorrents.pl
-lftp -f lftp.script
-ServerTorrents.pl
+BASEDIR=/home/OpenBSDTorrents
+TORRENTDIR=/home/torrentsync/torrents
+
+cd ${BASEDIR}
+
+if [[ $1 != skip ]]; then
+	echo ${BASEDIR}/MakeTorrents.pl $*
+	${BASEDIR}/MakeTorrents.pl $*
+fi
+
+echo ${BASEDIR}/CurrentTorrents.pl
+${BASEDIR}/CurrentTorrents.pl
+
+if [ $? != 253 ]; then
+	echo lftp -f ${BASEDIR}/lftp.script
+	lftp -f ${TORRENTDIR}/lftp.script
+
+	echo ${BASEDIR}/ServerTorrents.pl
+	${BASEDIR}/ServerTorrents.pl
+fi
