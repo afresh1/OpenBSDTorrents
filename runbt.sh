83f796b05b3ee7d043f6de61b59e4b28f37cf803
andrew <andrew>
2005-03-28 23:47:21 +0000

    added logging

--- runbt.sh
+++ runbt.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 #$Id$
 
+LOGFILE=runbt.log
+
+echo -n `date` >> ${LOGFILE}
+echo 	Starting . . .  >> ${LOGFILE}
+
 btlaunchmany.py \
     --super_seeder 1 \
     --minport 10000 --maxport 10500 \
     --saveas /home/ftp/pub/ --saveas_style 2 \
-    /home/torrentsync/torrents/
+    /home/torrentsync/torrents/ > /dev/null
+
+echo -n `date` >> ${LOGFILE}
+echo 	Died . . .  >> ${LOGFILE}
 
 /home/OpenBSDTorrents/OpenBSDTorrents/runbt.sh &
