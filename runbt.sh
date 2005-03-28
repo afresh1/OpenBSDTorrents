2a61d931f4572609d860219e1c8f9b19ad1e3784
andrew <andrew>
2005-03-28 23:13:41 +0000

    moved to new home

--- runbt.sh
+++ runbt.sh
@@ -5,6 +5,6 @@ btlaunchmany.py \
     --super_seeder 1 \
     --minport 10000 --maxport 10500 \
     --saveas /home/ftp/pub/ --saveas_style 2 \
-    /home/andrew/torrents/
+    /home/torrentsync/torrents/
 
-/home/andrew/OpenBSDTorrents/runbt.sh &
+/home/OpenBSDTorrents/OpenBSDTorrents/runbt.sh &
