51be48cf7144ba54fc276e91d576c24144047cca
andrew <andrew>
2005-04-29 20:16:31 +0000

    changed the logging, and the max_open_files

--- runbt.sh
+++ runbt.sh
@@ -9,8 +9,9 @@ echo '	Starting . . . ' >> ${LOGFILE}
 btlaunchmany.py \
     --super_seeder 1 \
     --minport 10000 --maxport 10500 \
+    --max_files_open 75 \
     --saveas /home/ftp/pub/ --saveas_style 2 \
-    /home/torrentsync/torrents/ > /dev/null
+    /home/torrentsync/torrents/ >> ${LOGFILE}
 
 echo -n `date` >> ${LOGFILE}
 echo '	Died . . . ' >> ${LOGFILE}
