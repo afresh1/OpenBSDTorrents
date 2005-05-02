2939cb18ad3f7dd405bf13e142ca11abab3ba1f4
andrew <andrew>
2005-05-02 21:43:53 +0000

    Now uses the OBTMetaInfo so I have info_hash_cached and it moves the meta info around with the torrent and it creates stuff in OBT_DIR_NEW_TORRENT and moves it later so we don't have copies of the same thing being seeded

--- CurrentTorrents.pl
+++ CurrentTorrents.pl
@@ -4,15 +4,15 @@ use strict;
 use warnings;
 use diagnostics;
 
-use BT::MetaInfo;
 use Time::Local;
 
 use lib 'lib';
 use OpenBSDTorrents;
+use BT::OBTMetaInfo;
 
 %ENV = ();
 
-#use YAML;
+use YAML;
 
 justme();
 
@@ -24,84 +24,104 @@ if ($Name_Filter =~ /^(\w*)$/) {
 }
 
 my %files;
-opendir DIR, $OBT->{DIR_TORRENT} 
-	or die "Couldn't opendir $OBT->{DIR_TORRENT}: $!";
-foreach (readdir DIR) {
-	if (/^([^\/]+)$/) {
-		$_ = $1;
-	} else {
-		die "Invalid character in $_: $!";
+foreach my $DIR ($OBT->{DIR_NEW_TORRENT}, $OBT->{DIR_TORRENT}) {
+	opendir DIR, $DIR 
+		or die "Couldn't opendir $DIR: $!";
+	foreach (readdir DIR) {
+		next unless my ($ext) = /\.(torrent|$OBT->{META_EXT})$/;
+
+		if (/^([^\/]+)$/) {
+			$_ = $1;
+		} else {
+			die "Invalid character in $_: $!";
+		}
+		my ($name, $year, $mon, $mday, $hour, $min) = 
+		   /^(.*)-(\d{4})-(\d{2})-(\d{2})-(\d{2})(\d{2})/;
+
+		$mon--;
+		my $epoch = timegm(0,$min,$hour,$mday,$mon,$year);
+
+		#print "Adding $_\n";
+
+		$files{$ext}{$name}{$epoch} = {
+			file      => $_,
+			dir       => $DIR,
+			path      => "$DIR/$_",
+			ext       => $ext,
+			year      => $year,
+			mon       => $mon,
+			mday      => $mday,
+			hour      => $hour,
+			min       => $min,
+			name      => $name,
+			epoch     => $epoch,
+		};
+
 	}
-	next unless /\.torrent$/;
-	my ($name, $year, $mon, $mday, $hour, $min) = 
-	   /^(.*)-(\d{4})-(\d{2})-(\d{2})-(\d{2})(\d{2})/;
-
-	$mon--;
-	my $epoch = timegm(0,$min,$hour,$mday,$mon,$year);
-
-	#print "Adding $_\n";
-
-	$files{$name}{$epoch} = {
-		file      => $_,
-		year      => $year,
-		mon       => $mon,
-		mday      => $mday,
-		hour      => $hour,
-		min       => $min,
-		epoch     => $epoch,
-	};
-
+	closedir DIR;
 }
-closedir DIR;
 
 my %keep;
 my @delete;
-foreach my $name (keys %files) {
+foreach my $name (keys %{ $files{torrent} }) {
 	next unless $name =~ /^$Name_Filter/;
 	print "Checking $name\n";
 
-	foreach my $epoch ( sort { $b <=> $a } keys %{ $files{$name} } ) {
+	foreach my $epoch ( sort { $b <=> $a } keys %{ $files{torrent}{$name} } ) {
 		#print "\t$epoch\n";
-		my $torrent = $files{$name}{$epoch}{file};
+		my $torrent = $files{torrent}{$name}{$epoch}{path};
 
 		my $t;
-		eval { $t = BT::MetaInfo->new($OBT->{DIR_TORRENT} . "/$torrent"); };
+		eval { $t = BT::OBTMetaInfo->new( $torrent ); };
+
 		if ($@) {
 			warn "Error reading torrent $torrent\n";
 			next;
 		}
 
-		$files{$name}{$epoch}{comment}   = $t->{comment};
+		$files{torrent}{$name}{$epoch}{comment}   = $t->{comment};
 		my ($path) = $t->{comment} =~ /Files from ([^\n]+)\n/s;
 
 		unless (-d $OBT->{DIR_FTP} . "/$path") {
-			#print "Deleting $files{$name}{$epoch}{file} the path doesn't exist.\n"; 
-			push @delete, $files{$name}{$epoch}{file};
+			#print "Deleting $files{torrent}{$name}{$epoch}{file} the path doesn't exist.\n"; 
+			push @delete, $files{torrent}{$name}{$epoch};
+			delete $files{torrent}{$name}{$epoch};
+			next;
 		}
 
-		if (keys %{ $files{$name} } == 1) {
+		if (
+			keys %{ $files{torrent}{$name} } == 1 &&
+			$files{torrent}{$name}{$epoch}{dir} 
+				eq $OBT->{DIR_TORRENT}
+		) {
 			#print "Skipping torrent for $name there is only one.\n";
 			next;
 		}
 
-		my $hash = $t->info_hash;
+		my $hash = $t->info_hash_cached($torrent);
 		$hash = unpack("H*", $hash);
 
-		$files{$name}{$epoch}{info_hash} = $hash;
+		$files{torrent}{$name}{$epoch}{info_hash} = $hash;
 
 		undef $t;
 
 		if (exists $keep{$name}) {
 			if (exists $keep{$name}{$hash}) {
 				push @delete, $keep{$name}{$hash};
+				delete $files{torrent}{
+					$keep{$name}{$hash}{name}
+				}{
+					$keep{$name}{$hash}{epoch}
+				};
 				$keep{$name}{$hash} = 
-					$files{$name}{$epoch}{file};
+					$files{torrent}{$name}{$epoch};
 			} else {
-				push @delete, $files{$name}{$epoch}{file};
+				push @delete, $files{torrent}{$name}{$epoch};
+				delete $files{torrent}{$name}{$epoch};
 			}
 		} else { 
 			$keep{$name}{$hash} = 
-				$files{$name}{$epoch}{file};
+				$files{torrent}{$name}{$epoch};
 
 		}
 	}
@@ -110,6 +130,42 @@ foreach my $name (keys %files) {
 #print Dump \%files, \%keep, \@delete;
 
 foreach (@delete) {
-	print "Deleting '$_'\n";
-	unlink $OBT->{DIR_TORRENT} . "/$_" or die "Couldn't unlink $_";
+	print "Deleting '$_->{path}'\n";
+	unlink $_->{path} or die "Couldn't unlink $_->{path}";
+}
+
+foreach my $name (keys %{ $files{$OBT->{META_EXT} } }) {
+	foreach my $epoch (keys %{ $files{ $OBT->{META_EXT} }{$name} }) {
+		unless ( exists $files{torrent}{$name}{$epoch} ) {
+			my $path = $files{$OBT->{META_EXT}}{$name}{$epoch}{path};
+			print "Unlinking '$path'\n";
+			unlink $path or die "couldn't unlink '$path': $!";
+		}
+	}
+}
+
+
+#print Dump \%keep;
+foreach my $name (keys %keep) {
+	foreach my $hash (keys %{ $keep{$name} }) {
+		my $file = $keep{$name}{$hash}{file};
+		my $dir  = $keep{$name}{$hash}{dir };
+		if ($dir eq $OBT->{DIR_NEW_TORRENT}) {
+			print "Moving $file to current torrents\n";
+			rename("$dir/$file", $OBT->{DIR_TORRENT} . "/" . $file)
+				or die "Couldn't rename '$file': $!";
+
+			my $name = $keep{$name}{$hash}{name};
+			my $epoch = $keep{$name}{$hash}{epoch};
+
+			if (exists $files{txt}{$name}{$epoch}) { 
+				my $m_file = $files{txt}{$name}{$epoch}{file};
+				my $m_dir  = $files{txt}{$name}{$epoch}{dir };
+				rename(
+					"$m_dir/$m_file", 
+					$OBT->{DIR_TORRENT} . "/" . $m_file
+				) or die "Couldn't rename '$m_file': $!";
+			}
+		}
+	}
 }
