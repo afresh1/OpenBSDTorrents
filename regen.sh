7ffdc04fb8ad2f911f9b936a0cba920e2e4c5a90
andrew <andrew>
2005-05-02 21:49:21 +0000

    make it only run once

--- regen.sh
+++ regen.sh
@@ -4,6 +4,17 @@
 . /etc/OpenBSDTorrents.conf
 
 cd ${OBT_DIR_HOME}
+PIDFILE=${OBT_DIR_HOME}/run/regen.pid
+
+if [ -e ${PIDFILE} ]; then
+        PID=`head -1 ${PIDFILE}`
+        kill -0 ${PID} 2> /dev/null
+        if [ $? -eq 0 ]; then
+                echo Already running
+                exit 1
+        fi
+fi
+echo $$ > ${PIDFILE}
 
 if [[ $1 != skip ]]; then
 	echo ${OBT_DIR_HOME}/MakeTorrents.pl $*
