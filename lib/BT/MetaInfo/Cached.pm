6f3383ea8582a7546f1251e2a61f9fae13f7aad2
andrew <andrew>
2005-05-02 19:43:44 +0000

    just to get a version number in the Id

--- lib/BT/MetaInfo/Cached.pm
+++ lib/BT/MetaInfo/Cached.pm
@@ -1,20 +1,27 @@
+# $Id$
+use strict;
+
 package BT::OBTMetaInfo;
 
 require 5.6.0;
-use strict;
+use vars qw( $VERSION @ISA );
 
 use BT::MetaInfo;
 
-$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+use Data::Dumper;
+
+$VERSION = do { my @r = (q$Id$ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
 sub new
 {
 	my $classname	= shift;
-	my $pass = shift;
+	print Dumper $classname;
+	exit;
 
-	my $self	= $classname->SUPER::new(@_);
+#	my $self	= $classname->SUPER::new(@_);
 }	
 
 
 sub info_hash { return(sha1(bencode($_[0]->info))); }
 
+1
