64a13a2588bd6a63311b436e38045c4f9bf0d203
andrew <andrew>
2005-05-18 00:17:12 +0000

    new features

--- runbt.sh
+++ runbt.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 #$Id$
 
-LOGFILE=runbt.log
+. /etc/OpenBSDTorrents.conf
+LOGFILE=/home/torrentseeder/runbt.log
+
+cd ${OBT_DIR_HOME}
+PIDFILE=${OBT_DIR_HOME}/run/runbt.pid
+
+if [ -e ${PIDFILE} ]; then
+        PID=`head -1 ${PIDFILE}`
+        kill -0 ${PID} 2> /dev/null
+        if [ $? -eq 0 ]; then
+                echo Already running
+                exit 1
+        fi
+fi
+echo $$ > ${PIDFILE}
+
 
 echo -n `date` >> ${LOGFILE}
 echo '	Starting . . . ' >> ${LOGFILE}
 
-btlaunchmany.py \
+nice btlaunchmany.py \
     --super_seeder 1 \
-    --minport 10000 --maxport 10500 \
+    --check_hashes 0 \
+    --display_interval 60 \
+    --minport 6881 --maxport 6989 \
     --max_files_open 75 \
     --saveas /home/ftp/pub/ --saveas_style 2 \
     /home/torrentsync/torrents/ >> ${LOGFILE}
 
 echo -n `date` >> ${LOGFILE}
 echo '	Died . . . ' >> ${LOGFILE}
-
-/home/OpenBSDTorrents/runbt.sh &
