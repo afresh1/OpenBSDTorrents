41f7824ba2699f717e501903ccdd51c7a063e939
Andrew Fresh <andrew@afresh1.com>
2012-09-08 17:18:48 -0700

    perltidy
    So many tabs!

--- NewTorrents.pl
+++ NewTorrents.pl
@@ -15,111 +15,120 @@ my %Need_Update;
 
 %ENV = ();
 
-my $last_dir = '';
+my $last_dir  = '';
 my $last_file = '';
 while (<>) {
-	#print;
-	chomp;
-	# *** This requires --log-format="%t [%p] %o %f %l" on the rsync command
-	if (my ($year,  $mon,   $mday,     $time,
-                $pid,        $oper,    $file,  $size) = m#^
+
+    #print;
+    chomp;
+
+    # *** This requires --log-format="%t [%p] %o %f %l" on the rsync command
+    if (my ( $year, $mon, $mday, $time, $pid, $oper, $file, $size )
+        = m#^
 		(\d{4})/(\d{2})/(\d{2}) \s (\d{2}:\d{2}:\d{2}) \s
 		\[(\d+)\] \s (\S+) \s  (.+) \s (\d+)
-		$#xms) {
+		$#xms
+        )
+    {
 
-		$file =~ s/^.*$OBT->{BASENAME}\/?//;
+        $file =~ s/^.*$OBT->{BASENAME}\/?//;
 
-        	my ($dir, $file) = $file =~ m#^(.*)/([^/]+)#;
-		#print "$oper - ($last_dir) [$dir]/[$file]\n";
+        my ( $dir, $file ) = $file =~ m#^(.*)/([^/]+)#;
 
-		next unless $oper eq 'recv';
-		next unless $size;
-		next unless $dir;
+        #print "$oper - ($last_dir) [$dir]/[$file]\n";
 
-		if ($last_dir && $last_dir ne $dir) {
-			StartTorrent($last_dir);
-		}
-		elsif ($last_file && $last_file ne $file 
-			&& $last_file =~ /$INSTALL_ISO_REGEX/xms) {
-			StartTorrent("$dir/$file");
-		}
+        next unless $oper eq 'recv';
+        next unless $size;
+        next unless $dir;
 
-		$last_dir = $dir;
-		$last_file = $file;
-	}
+        if ( $last_dir && $last_dir ne $dir ) {
+            StartTorrent($last_dir);
+        }
+        elsif ($last_file
+            && $last_file ne $file
+            && $last_file =~ /$INSTALL_ISO_REGEX/xms )
+        {
+            StartTorrent("$dir/$file");
+        }
+
+        $last_dir  = $dir;
+        $last_file = $file;
+    }
 }
 
 # Regen just the new ones now
-sleep(1) while (keys %Kids > 0);
+sleep(1) while ( keys %Kids > 0 );
 StartTorrent($last_dir);
 
 # after the new ones are done, regen all, just to make sure
-sleep(1) while (keys %Kids > 0);
+sleep(1) while ( keys %Kids > 0 );
 StartTorrent('skip');
 
 # and wait for it to finish
-sleep(1) while (keys %Kids > 0);
+sleep(1) while ( keys %Kids > 0 );
 
 sub REAPER {
-	my $child;
-        while (($child = waitpid(-1,WNOHANG)) > 0) {
-		$Kid_Status{$child} = $?;
-		delete $Kids{$child};
-	}
-	$SIG{CHLD} = \&REAPER;  # still loathe sysV
+    my $child;
+    while ( ( $child = waitpid( -1, WNOHANG ) ) > 0 ) {
+        $Kid_Status{$child} = $?;
+        delete $Kids{$child};
+    }
+    $SIG{CHLD} = \&REAPER;    # still loathe sysV
 
-	StartTorrent('waiting');
+    StartTorrent('waiting');
 }
 
-sub StartTorrent
-{
-	my $dir = shift;
-	return undef unless $dir;
-	
-	my $should_fork = 1;
+sub StartTorrent {
+    my $dir = shift;
+    return undef unless $dir;
 
-	if ($dir eq 'skip') {
-		#$dir = '';
-		%Need_Update = ();
-		$should_fork = 0;
-	} 
-	elsif ($dir eq 'waiting') {
-		return if ! %Need_Update;
+    my $should_fork = 1;
 
-		my $count = scalar keys %Need_Update;
-		print "Have $count waiting torrents\n";
-	}
-	else {
-		#print "Need to make torrent for '$dir'\n";
-		$dir = $OBT->{BASENAME} . "/$dir";
-		$Need_Update{$dir} = 1;
-	}
+    if ( $dir eq 'skip' ) {
 
-	if (keys %Kids > 0) {
-		print "Not making torrents for $dir now, already running\n";
-		return;
-	}
+        #$dir = '';
+        %Need_Update = ();
+        $should_fork = 0;
+    }
+    elsif ( $dir eq 'waiting' ) {
+        return if !%Need_Update;
 
-	my @now_update = keys %Need_Update;
-	%Need_Update = ();
+        my $count = scalar keys %Need_Update;
+        print "Have $count waiting torrents\n";
+    }
+    else {
 
-	if ($should_fork) {
-		defined(my $pid = fork)	or die "Can't fork: $!";
+        #print "Need to make torrent for '$dir'\n";
+        $dir = $OBT->{BASENAME} . "/$dir";
+        $Need_Update{$dir} = 1;
+    }
 
-		if ($pid) {
-			$Kids{$pid} = 1;
-			return;
-		}
+    if ( keys %Kids > 0 ) {
+        print "Not making torrents for $dir now, already running\n";
+        return;
+    }
 
-	}
+    my @now_update = keys %Need_Update;
+    %Need_Update = ();
 
-	print "\n";
-	if (@now_update) {
-		print "Making torrents for ", join(" ", @now_update), "\n";
-	} else {
-		print "Remaking all torrents\n";
-		push @now_update, $dir;
-	}
-	exec($OBT->{DIR_HOME} . '/regen.sh', @now_update);
-	exit;
+    if ($should_fork) {
+        defined( my $pid = fork ) or die "Can't fork: $!";
+
+        if ($pid) {
+            $Kids{$pid} = 1;
+            return;
+        }
+
+    }
+
+    print "\n";
+    if (@now_update) {
+        print "Making torrents for ", join( " ", @now_update ), "\n";
+    }
+    else {
+        print "Remaking all torrents\n";
+        push @now_update, $dir;
+    }
+    exec( $OBT->{DIR_HOME} . '/regen.sh', @now_update );
+    exit;
 }
