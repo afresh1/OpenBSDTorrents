4508123d22706488c9486e0764a817554fc16bb4
Andrew Fresh <andrew@afresh1.com>
2012-10-13 18:14:31 -0700

    Make it a bit more generic so "anyone" can seed torrents
    It should work for pretty much anyone which is nice.
    
    It doesn't keep track of torrents IT added so it does just remove
    any unrecognized torrents but for me that works.

--- SeedTorrents.pl
+++ SeedTorrents.pl
@@ -2,19 +2,31 @@
 use strict;
 use warnings;
 
-use Transmission::Client;
-use Transmission::Utils;
+use Mojo::UserAgent;
 
-my %keep;
-my %seeding;
+#use Transmission::Utils;
+use Transmission::Client;
+
+my $torrent_uri = 'http://openbsd.somedomain.net/torrent/';
+my $download_dir = '';    # If you want to override
+
+use lib 'lib';
+eval { require OpenBSDTorrents };
+unless ($@) {
+    $torrent_uri  = $OpenBSDTorrents::OBT->{URL_TORRENTS};
+    $download_dir = $OpenBSDTorrents::OBT->{DIR_FTP};
+}
+
+my $current
+    = Mojo::UserAgent->new->get( $torrent_uri . 'torrents.json' )->res->json;
+die "Couldn't get current torrents" unless $current;
 
 my $client = Transmission::Client->new;
-foreach my $torrent (@{ $client->torrents } ) {
-
-    #my $status = Transmission::Utils::from_numeric_status($torrent->status);
+foreach my $torrent ( @{ $client->torrents } ) {
     my $hash = $torrent->hash_string;
-    if ( exists $keep{$hash} ) {
-        $seeding{$hash} = $torrent;
+
+    if ( exists $current->{$hash} ) {
+        delete $current->{$hash};    # No need to do anything
     }
     else {
         print "No longer seeding [$hash]\n";
@@ -23,19 +35,14 @@ foreach my $torrent (@{ $client->torrents } ) {
     }
 }
 
+foreach my $hash ( keys %{$current} ) {
+    my $file = $current->{$hash}{torrent};
+    print "Starting seed of [$file] [$hash]\n";
 
-foreach my $hash ( keys %keep ) {
-    my $file = $keep{$hash}{file} || q{};
-    my $dir  = $keep{$hash}{dir}  || q{};
-
-    if ( !$seeding{$hash} ) {
-        print 'Starting seed of ' . $reason . "[$file] [$hash]\n";
-        $client->add(
-            filename     => "$dir/$file",
-            download_dir => $OBT->{DIR_FTP},
-        ) ||  warn $client->error, ": $dir/$file\n";
-    }
+    $client->add(
+        filename     => $torrent_uri . $file,
+        download_dir => $download_dir,
+    ) || warn $client->error, ": $file\n";
 }
-
 
 $client->start;
--- regen.sh
+++ regen.sh
@@ -27,5 +27,5 @@ echo ${OBT_DIR_HOME}/CurrentTorrents.pl
 ${OBT_DIR_HOME}/CurrentTorrents.pl
 
 echo 
-echo ${OBT_DIR_HOME}/SeedTorrents[.pl
+echo ${OBT_DIR_HOME}/SeedTorrents.pl
 ${OBT_DIR_HOME}/SeedTorrents.pl
