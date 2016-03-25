af1376c2359c85179237a07fe21df2a1f4e8780d
Andrew Fresh <andrew@afresh1.com>
2016-03-24 19:33:35 -0700

    Add a script to run 'rtorrent' to serve torrents

---
+++ start_seeder.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo -u _torrentseeder tmux new-session -d 'rtorrent \
+	-d /var/www/ftp/pub/ \
+	-s /var/torrentseeder/ \
+	-o dht=on,upload_rate=4096 \
+	-Oschedule=load,10,10,load_start=/var/www/ftp/pub/torrents/*.torrent \
+	-Oschedule=untie,10,10,remove_untied='
